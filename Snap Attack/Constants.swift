474590eb632ea86264ba9ddb592fb4d12ba84585
apramodya <pramodyaabeysinghe@gmail.com>
2019-03-29 03:16:09 +0800

    implement game functionality

---
+++ Snap Attack/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Snap Attack
+//
+//  Created by Pramodya Abeysinghe on 29/3/19.
+//  Copyright © 2019 Pramodya. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let MAX_TIME = 10
+    static let CARD_LIST = [
+        "ace_of_hearts",
+        "2_of_hearts",
+        "3_of_hearts",
+        "4_of_hearts",
+        "5_of_hearts",
+        "6_of_hearts",
+        "7_of_hearts",
+        "8_of_hearts",
+        "9_of_hearts",
+        "10_of_hearts",
+    ]
+}
--- Snap Attack/ViewController.swift
+++ Snap Attack/ViewController.swift
@@ -16,12 +16,74 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondCard: UIImageView!
     @IBOutlet weak var startBtn: UIButton!
     
+    var timer = Timer()
+    var cardTimer = Timer()
+    var timerInt = Constants.MAX_TIME
+    var scoreInt = 0
+    var gameMode = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        timeLbl.text = String("Time: \(Constants.MAX_TIME)")
     }
 
     @IBAction func startGame(_ sender: Any) {
+        if timerInt == Constants.MAX_TIME {
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            cardTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCards), userInfo: nil, repeats: true)
+            startBtn.isEnabled = false
+            startBtn.alpha = 0.25
+            startBtn.setTitle("Snap", for: .normal)
+        }
+        
+        if gameMode == 1 {
+            if firstCard.image == secondCard.image {
+                scoreInt += 1
+                scoreLbl.text = String("Score: \(scoreInt)")
+            } else {
+                scoreInt -= 1
+                scoreLbl.text = String("Score: \(self.scoreInt)")
+            }
+        }
+        
+        if timerInt == 0 {
+            timerInt = Constants.MAX_TIME
+            scoreInt = 0
+            
+            timeLbl.text = String("Time: \(timerInt)")
+            scoreLbl.text = String("Score: \(self.scoreInt)")
+            
+            startBtn.setTitle("Start", for: .normal)
+            
+            firstCard.image = UIImage(named: "blue_cover")
+            secondCard.image = UIImage(named: "blue_cover")
+        }
+    }
+    
+    @objc func updateTimer() {
+        timerInt -= 1
+        gameMode = 1
+        
+        timeLbl.text = String("Time: \(timerInt)")
+        startBtn.isEnabled = true
+        startBtn.alpha = 1
+        
+        if timerInt == 0 {
+            timer.invalidate()
+            cardTimer.invalidate()
+            
+            gameMode = 0
+            startBtn.setTitle("Restart", for: .normal)
+        }
+    }
+    
+    @objc func updateCards() {
+        let cards = Constants.CARD_LIST
+        
+        firstCard.image = UIImage(named: cards.randomElement()!)
+        secondCard.image = UIImage(named: cards.randomElement()!)
     }
     
 }
